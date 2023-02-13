29cb3b5f25f20130fca72238cbc0017708953464
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-02-13 13:52:12 -0300

    Att:: change post return

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -67,13 +67,13 @@ export async function transactionsRoutes(app: FastifyInstance) {
       })
     }
 
-    const transaction = await knex('transactions').insert({
+    await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
       session_id: sessionId,
     })
-    return reply.status(201).send({ transaction })
+    return reply.status(201).send()
   })
 
   app.get(
